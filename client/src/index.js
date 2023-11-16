7ddd1be4cfd9935eb462a6eae8bc0fc6ea1c8817
root <root@LucasRamos>
2023-11-16 19:01:11 -0300

    Atualizando dependencias do projeto

--- client/src/index.js
+++ client/src/index.js
@@ -25,8 +25,6 @@ const routes = createBrowserRouter([
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
     <RouterProvider router={routes}/>
-  </React.StrictMode>
 );
 
