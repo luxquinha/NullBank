8c9b7af5502db5110d9c691adbf38d780b356c7e
pauloabraao <abraaoteles@alu.ufc.br>
2023-11-22 15:54:42 -0300

    added update and delete feature

--- client/src/pages/admin/Agency.jsx
+++ client/src/pages/admin/Agency.jsx
@@ -1,114 +1,53 @@
-import React from "react";
-import axios from "axios";
+
 import styled from "styled-components";
-import { FaTrash, FaEdit } from "react-icons/fa";
-import { useEffect, useState } from "react";
-import "react-toastify/dist/ReactToastify.css";
-import { toast } from "react-toastify";
+import Form from "./FormsAgency";
+import TableAgency from "./TableAgency";
+import { toast, ToastContainer } from "react-toastify";
+import GlobalStyle from "../../styles/global";
+import React, { useEffect, useRef, useState } from "react";
+import axios from "axios";
 
-const Table = styled.table`
-  width: 100%;
-  background-color: #fff;
-  padding: 20px;
-  box-shadow: 0px 0px 5px #ccc;
-  border-radius: 5px;
-  max-width: 1120px;
-  margin: 20px auto;
-  word-break: break-all;
+
+const Container = styled.div`
+  width: auto;
+  max-width: 800px;
+  margin-top: 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 10px;
 `;
 
-export const Thead = styled.thead``;
+const Title = styled.h2``;
 
-export const Tbody = styled.tbody``;
-
-export const Tr = styled.tr``;
-
-export const Th = styled.th`
-  text-align: start;
-  border-bottom: inset;
-  padding-bottom: 5px;
-
-  @media (max-width: 500px) {
-    ${(props) => props.onlyWeb && "display: none"}
-  }
-`;
-
-export const Td = styled.td`
-  padding-top: 15px;
-  text-align: ${(props) => (props.alignCenter ? "center" : "start")};
-  width: ${(props) => (props.width ? props.width : "auto")};
-
-  @media (max-width: 500px) {
-    ${(props) => props.onlyWeb && "display: none"}
-  }
-`;
-
-const Agency = () => {
-  const [users, setUsers] = useState([]);
-  const [onEdit, setOnEdit] = useState(null);
-
-  const getUsers = async () => {
-    try {
-      const res = await axios.get("http://localhost:8800");
-      setUsers(res.data.sort((a, b) => (a.nome > b.nome ? 1 : -1)));
-    } catch (error) {
-      toast.error(error);
-    }
-  };
-
-  useEffect(() => {
-    getUsers();
-  }, [setUsers]);
-
-
-  const handleEdit = (item) => {
-    setOnEdit(item);
-  };
-
-  const handleDelete = async (numero) => {
-    await axios
-      .delete("http://localhost:8800/" + numero)
-      .then(({ data }) => {
-        const newArray = users.filter((user) => user.numero !== numero);
-
-        setUsers(newArray);
-        toast.success(data);
-      })
-      .catch(({ data }) => toast.error(data));
-
-    setOnEdit(null);
-  };
+function Agency() {
+    const [users, setUsers] = useState([]);
+    const [onEdit, setOnEdit] = useState(null);
+  
+    const getUsers = async () => {
+      try {
+        const res = await axios.get("http://localhost:8800");
+        setUsers(res.data.sort((a, b) => (a.nome > b.nome ? 1 : -1)));
+      } catch (error) {
+        toast.error(error);
+      }
+    };
+  
+    useEffect(() => {
+      getUsers();
+    }, [setUsers]);
 
   return (
-    <Table>
-      <Thead>
-        <Tr>
-          <Th>Numero</Th>
-          <Th>Nome</Th>
-          <Th onlyWeb>Cidade</Th>
-          <Th></Th>
-          <Th></Th>
-        </Tr>
-      </Thead>
-      <Tbody>
-        {users.map((item, i) => (
-          <Tr key={i}>
-            <Td width="30%">{item.numero}</Td>
-            <Td width="30%">{item.nome}</Td>
-            <Td width="20%" onlyWeb>
-              {item.cidade}
-            </Td>
-            <Td alignCenter width="5%">
-              <FaEdit onClick={() => handleEdit(item)} />
-            </Td>
-            <Td alignCenter width="5%">
-              <FaTrash onClick={() => handleDelete(item.numero)} />
-            </Td>
-          </Tr>
-        ))}
-      </Tbody>
-    </Table>
+    <>
+      <Container>
+        <Title>AGÊNCIAS</Title>
+        <Form  onEdit={onEdit} setOnEdit={setOnEdit} getUsers={getUsers}/>
+        <TableAgency setOnEdit={setOnEdit} users={users} setUsers={setUsers} />
+      </Container>
+      <ToastContainer autoClose={3000} position={toast.POSITION.BOTTOM_LEFT} />
+      <GlobalStyle />
+    </>
   );
-};
+}
 
 export default Agency;
