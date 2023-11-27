dd30290387f26f58df42342972492dd29f7ef377
root <root@LucasRamos>
2023-11-26 21:09:21 -0300

    Unificando páginas de login

--- client/src/pages/paginas_iniciais/SingIn.jsx
+++ client/src/pages/paginas_iniciais/SingIn.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import { Link } from "react-router-dom";
 import { useForm } from "react-hook-form";
 import { z } from 'zod'
@@ -12,13 +12,18 @@ import ErrorMessage from "../../componentes/ErrorMessage";
 import { seta } from '../../icons/icones'
 
 const loginAcessSchema = z.object({
-    cpf: z.string().length(11, {message: 'Digite um cpf válido'}),
-    password: z.string().min(6, {message: 'Deve conter 6 caracteres'})
+    key: z.string(),
+    password: z.string().min(4, {message: 'Minimo 4 caracteres'})
 }).refine(data => data.cpf !== undefined || data.password !== undefined, {
     message: 'Campo obrigatório'
 })
 
 export default function SingIn(){
+    const adminUser = {
+        user: 'Admin',
+        password: '1234'
+    }
+    const [tipoUsuario, setTipoUsuario] = useState('dba')
     const irPara = useNavigate()
     const fundo = {
         backgroundImage: "url('https://media.istockphoto.com/id/1442295199/pt/foto/successful-financier-investor-works-inside-office-at-work-businessman-in-business-suit-uses.jpg?s=1024x1024&w=is&k=20&c=0SNAhOhTTARN8a85GRk9AtNgl5dF0RFK7NvvZhNGEw8=')"
@@ -26,9 +31,22 @@ export default function SingIn(){
     const { register, handleSubmit, formState: { errors } } = useForm({
         resolver: zodResolver(loginAcessSchema)
     })
+
+    const isAdmin = (data)=>{
+        return (data.key === adminUser.user && data.password === adminUser.password)
+    }
+
     const onSubmit = (data)=>{
-        console.log(data)
-        irPara('/admin')
+        // Condicional que vai dizer qual pagina deve ser redicionada para o usuário:
+        if(isAdmin(data))
+            irPara('/admin')
+        else if(tipoUsuario === 'funcionario')
+            // Função que verifica se é um funcionário dentro de uma condicional
+            irPara('/funcionario')
+        // Caso seja um cliente:
+        else if(tipoUsuario === 'cliente')
+            // Função que verifica se é um cliente dentro de uma condicional
+            irPara('/home')
     }
     return(
         <div className="flex flex-row items-center w-full">
@@ -39,11 +57,17 @@ export default function SingIn(){
             </div>
             <div className="h-screen w-[50%] flex flex-col">
                 <div className="w-[88%] h-[80%] flex flex-col p-10 items-center self-center justify-center gap-2">
-                    <span className="text-3xl font-semibold self-start ml-[13%] mb-3">Acesse sua conta</span>
+                    <span className="text-3xl font-semibold self-start ml-[13%] mb-3">
+                        {(tipoUsuario === 'funcionario') ? 'Seja bem-vindo Funcionário' : (tipoUsuario === 'cliente') ? 
+                        'Acesse sua conta' : 'Seja bem-vindo Administrador'}
+                    </span>
                     <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col w-[75%] h-[50%] border-solid border-red-500 gap-y-6">
-                        <span className="self-start text-sm">CPF</span>
-                        <InputBorderBottom width={'full'} register={register} label={'cpf'}/>
-                        {errors.cpf && <ErrorMessage message={errors.cpf?.message}/>}
+                        <span className="self-start text-sm">
+                        {(tipoUsuario === 'funcionario') ? 'Matrícula' : (tipoUsuario === 'cliente') ? 
+                        'CPF' : 'Usuário'}
+                        </span>
+                        <InputBorderBottom width={'full'} register={register} label={'key'}/>
+                        {errors.key && <ErrorMessage message={errors.key?.message}/>}
                         <span className="self-start text-sm mt-4">Senha</span>
                         <InputBorderBottom width={'full'} type={'password'} register={register} label={'password'} />
                         {errors.cpf && <ErrorMessage message={errors.password?.message}/>}
@@ -51,9 +75,10 @@ export default function SingIn(){
                     </form>
                 </div>
                 <div className="bg-zinc-200 flex flex-row items-center justify-around w-full h-[20%]">
-                    <Link to={'/loginFuncionario'} className="no-underline font-semibold text-sm text-cyan-600 hover:text-cyan-500">Sou funcionário</Link>
-                    <Link to={'/loginAdmin'} className="no-underline font-semibold text-sm text-cyan-600 hover:text-cyan-500">Sou DBA</Link>
-                    <Link to={'/'} className="no-underline font-semibold text-sm text-cyan-600 hover:text-cyan-500">Não sou cliente</Link>
+                    <button className="no-underline font-semibold text-sm text-cyan-600 hover:text-cyan-500" onClick={()=>{setTipoUsuario('funcionario')}}>Sou funcionário</button>
+                    <button className="no-underline font-semibold text-sm text-cyan-600 hover:text-cyan-500" onClick={()=>{setTipoUsuario('cliente')}}>Sou Cliente</button>
+                    <button className="no-underline font-semibold text-sm text-cyan-600 hover:text-cyan-500" onClick={()=>{setTipoUsuario('dba')}}>Sou DBA</button>
+                    {/* <Link to={'/'} className="no-underline font-semibold text-sm text-cyan-600 hover:text-cyan-500">Não sou cliente</Link> */}
                 </div>
             </div>
         </div>
