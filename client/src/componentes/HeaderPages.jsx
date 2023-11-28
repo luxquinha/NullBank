5a6e62b74602084e9c15242e0076395be3cb61d4
root <root@LucasRamos>
2023-11-28 14:46:10 -0300

    Validações dos usuários

--- client/src/componentes/HeaderPages.jsx
+++ client/src/componentes/HeaderPages.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import useLoginContext from "../hooks/useLoginContext";
-
 import { Link } from "react-router-dom";
 import { logOut } from "../icons/icones";
 
